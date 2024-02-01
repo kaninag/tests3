ed575ff4fb928c47c83c8af5f4ea7647484fa7ab
kani <kaninag1401@gmail.com>
2024-02-01 11:48:05 +0530

    First commit

---
+++ first.js
@@ -0,0 +1,11 @@
+//var msg="hii this is js";
+//alert(msg);
+//console.log(msg);
+let name={
+    name:"kani",
+    age:22,
+gender:"female"
+}
+console.log(name.name)
+console.log(name['gender'])
+       
